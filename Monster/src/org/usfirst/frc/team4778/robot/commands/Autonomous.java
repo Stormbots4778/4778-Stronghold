4f274258cc58671862eb1dfa9f1449f87ea3dca8
Chase Reinertson <chasereinertson@gmail.com>
2016-02-20 18:53:01 -0600

    update for sharing between team

--- Monster/src/org/usfirst/frc/team4778/robot/commands/Autonomous.java
+++ Monster/src/org/usfirst/frc/team4778/robot/commands/Autonomous.java
@@ -26,7 +26,7 @@ public class Autonomous extends CommandGroup {
 		// arm.
 		System.out.println("-start auto");
 		addSequential(new Breach(true, 3));
-		addSequential(new TurnToAngle(90));
+		addSequential(new TurnToAngle(60));
 		System.out.println("-end auto");
 	}
 }
